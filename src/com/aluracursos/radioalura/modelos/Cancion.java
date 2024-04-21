8d285ff46030d1c85916e0e907462d3d742fab2b
luys <sanchezluys@gmail.com>
2024-04-21 09:47:01 -0500

    agregando la clase cancion, que hereda de la clase audio

---
+++ src/com/aluracursos/radioalura/modelos/Cancion.java
@@ -0,0 +1,22 @@
+package com.aluracursos.radioalura.modelos;
+
+public class Cancion extends Audio{
+    private String album;
+    private String genero;
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+}
